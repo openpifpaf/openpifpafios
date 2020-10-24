e38a6e127d2041d43f7bfffeab4ec42f38e31287
Sven Kreiss <me@svenkreiss.com>
2020-10-24 16:02:12 +0200

    Initial Commit

---
+++ openpifpafios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  openpifpafios
+//
+//  Created by Sven Kreiss on 24.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ openpifpafios/openpifpafiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  openpifpafiosApp.swift
+//  openpifpafios
+//
+//  Created by Sven Kreiss on 24.10.20.
+//
+
+import SwiftUI
+
+@main
+struct openpifpafiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
