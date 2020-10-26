121d8d226b2b6286370d0ee0f7522532fbc4669a
Sven Kreiss <me@svenkreiss.com>
2020-10-26 16:52:07 +0100

    camera preview

---
+++ openpifpafios/CameraViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CameraViewController.swift
+//  openpifpafios
+//
+//  Created by Sven Kreiss on 25.10.20.
+//
+
+import AVFoundation
+import UIKit
+import SwiftUI
+
+final class CameraViewController: UIViewController {
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // captureSession
+        captureSession = AVCaptureSession()
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput = try? AVCaptureDeviceInput(device: videoCaptureDevice)
+        captureSession.addInput(videoInput!)
+
+        // previewLayer
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+
+        captureSession.startRunning()
+    }
+}
+
+extension CameraViewController: UIViewControllerRepresentable {
+    public typealias UIViewControllerType = CameraViewController
+    
+    public func makeUIViewController(context: UIViewControllerRepresentableContext<CameraViewController>) -> CameraViewController {
+        return CameraViewController()
+    }
+    
+    public func updateUIViewController(_ uiViewController: CameraViewController, context: UIViewControllerRepresentableContext<CameraViewController>) {
+    }
+}
--- openpifpafios/ContentView.swift
+++ openpifpafios/ContentView.swift
@@ -9,8 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            CameraViewController()
+                .edgesIgnoringSafeArea(.top)
+            VStack {
+                Spacer()
+                Text("hello world")
+                    .foregroundColor(.white)
+                    .frame(minWidth: 200)
+                    .padding(.horizontal)
+                    .background(
+                        RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/)
+                    )
+            }
+        }
     }
 }
 
