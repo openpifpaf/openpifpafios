4e734246588f07cbe5bf24b1b49ee191a8e43bd4
Sven Kreiss <me@svenkreiss.com>
2021-04-01 19:34:02 +0200

    init pod libtorch

---
+++ openpifpafios/TorchBridge/TorchModule.h
@@ -0,0 +1,22 @@
+//
+//  TorchModule.h
+//  openpifpafios
+//
+//  from: https://github.com/pytorch/ios-demo-app/blob/master/HelloWorld/HelloWorld/HelloWorld/TorchBridge/
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TorchModule : NSObject
+
+- (nullable instancetype)initWithFileAtPath:(NSString*)filePath
+    NS_SWIFT_NAME(init(fileAtPath:))NS_DESIGNATED_INITIALIZER;
++ (instancetype)new NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
+- (nullable NSArray<NSNumber*>*)predictImage:(void*)imageBuffer NS_SWIFT_NAME(predict(image:));
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ openpifpafios/openpifpafios-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  openpifpafios-Bridging-Header.h
+//  openpifpafios
+//
+//  Created by Sven Kreiss on 01.04.21.
+//
+
+#import "TorchModule.h"
